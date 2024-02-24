e8aef68d8ea15d3be27e85f67072c25345a35773
pathummadhusanka <pathummadhusanka.dev@gmail.com>
2024-02-25 00:56:39 +0530

    Position pieces in their initial positions
    - Wrap each img in a div and append it to relevant square

--- js/script.js
+++ js/script.js
@@ -21,3 +21,56 @@ function createBoard() {
 }
 
 createBoard();
+
+function initPieces() {
+    const initPiecePositions = {
+        0: ['rook', 'dark'],
+        1: ['knight', 'dark'],
+        2: ['bishop', 'dark'],
+        3: ['king', 'dark'],
+        4: ['queen', 'dark'],
+        5: ['bishop', 'dark'],
+        6: ['knight', 'dark'],
+        7: ['rook', 'dark'],
+        8: ['pawn', 'dark'],
+        9: ['pawn', 'dark'],
+        10: ['pawn', 'dark'],
+        11: ['pawn', 'dark'],
+        12: ['pawn', 'dark'],
+        13: ['pawn', 'dark'],
+        14: ['pawn', 'dark'],
+        15: ['pawn', 'dark'],
+        48: ['pawn', 'light'],
+        49: ['pawn', 'light'],
+        50: ['pawn', 'light'],
+        51: ['pawn', 'light'],
+        52: ['pawn', 'light'],
+        53: ['pawn', 'light'],
+        54: ['pawn', 'light'],
+        55: ['pawn', 'light'],
+        56: ['rook', 'light'],
+        57: ['knight', 'light'],
+        58: ['bishop', 'light'],
+        59: ['king', 'light'],
+        60: ['queen', 'light'],
+        61: ['bishop', 'light'],
+        62: ['knight', 'light'],
+        63: ['rook', 'light']
+    };
+
+    const squares = document.querySelectorAll('.square');
+
+    squares.forEach((square, j) => {
+        if(initPiecePositions[j]) {
+            const pieceElement = document.createElement('div');
+            pieceElement.classList.add('piece');
+            pieceElement.setAttribute('draggable', 'true');
+
+            pieceElement.innerHTML = `<img src="./img/piece/${initPiecePositions[j][0]}-${initPiecePositions[j][1]}.webp" alt="${initPiecePositions[j][0]}-${initPiecePositions[j][1]}">`;
+
+            square.append(pieceElement);
+        }
+    });
+}
+
+initPieces();
